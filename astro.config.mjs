024f6a8d1ef8082cfcd2b0ce1cb5471144654d68
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2025-04-24 15:43:42 -0300

    eliminacion de base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Mauro11F.github.io',
-  base: 'Mauro11F.github.io',
   vite: {
       plugins: [tailwindcss()],
   },
