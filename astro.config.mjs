b313268576d62a6c37840f57fa4335f4de653e66
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2025-04-24 15:00:33 -0300

    Update astro.config.mjs
    Se agrego las recomendaciones de site: y base: que nos pide la documentacion Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Mauro11F.github.io',
+  base: 'my-repo',
   vite: {
       plugins: [tailwindcss()],
   },
